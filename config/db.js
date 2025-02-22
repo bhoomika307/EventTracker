dcb729665203c9ac132a0e158e573ee287e68491
bhoomika307 <123867523+bhoomika307@users.noreply.github.com>
2025-02-22 13:51:51 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-        apiKey: "AIzaSyD89oVsv-oPYMVTwGlSz2YFudJUKZxt95E",
-        authDomain: "eventtracker-c01b6.firebaseapp.com",
-        projectId: "eventtracker-c01b6",
-        storageBucket: "eventtracker-c01b6.appspot.com",
-        messagingSenderId: "378329853434",
-        appId: "1:378329853434:android:753a106e5dfae285120843",
+        apiKey: "$$$$$$$$$$$$$$$$$$$$$$",
+        authDomain: "$$$$$$$$$$$$$$$$$$",
+        projectId: "$$$$$$$$$$$$$$$$$$$",
+        storageBucket: "########",
+        messagingSenderId: "###########",
+        appId: "###############",
   };
   
   export default firebaseConfig;
